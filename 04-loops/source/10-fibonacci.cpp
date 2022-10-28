f83d66f4527efa927524687feb75bd1f4025ddaf
Zahari-M <107109124+Zahari-M@users.noreply.github.com>
2022-10-28 17:36:57 +0300

    Добавям решения на 4 задачи

---
+++ 04-loops/source/10-fibonacci.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n,fk = 1,fk_1 = 0; // съхраняват f(k) и f(k-1) съответно.
+						   // в началото приемаме, че k=1
+	std::cin >> n;
+
+	for (int k = 2; k <= n; k++)
+	{
+		int new_elem = fk + fk_1;  // новия елемент в редицата
+		fk_1 = fk;
+		fk = new_elem; // актуализираме последните 2 елемента
+	}
+
+	std::cout << fk << std::endl;
+
+}
