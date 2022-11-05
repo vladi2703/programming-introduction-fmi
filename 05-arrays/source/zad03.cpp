76e8631aee34e05a8d38b1bac44d655351619c6f
Zahari-M <107109124+Zahari-M@users.noreply.github.com>
2022-11-05 13:42:57 +0200

    Решения 1-5

---
+++ 05-arrays/source/zad03.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int MAX_NUMBER = 1000;
+	int count[MAX_NUMBER] = {}; // съхраняваме броя на срещанията на всяко число от 0 до 999
+	// count трябва да е нулиран
+
+	int N,number;
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> number; // дори не е нужно да съхраним самите числа в масив
+		count[number]++;
+	}
+
+	for (int i = 0; i < MAX_NUMBER; i++)
+		if (count[i] != 0)
+			std::cout << i << ": " << count[i] << std::endl;
+
+	for (int i = 0; i < MAX_NUMBER; i++)
+		for (int j = 0; j < count[i]; j++) // ако count[i] е 0, вътрешния цикъл се пропуска
+			std::cout << i << " ";
+
+	std::cout << std::endl;
+}
---
+++ 05-arrays/source/zad05.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int MAX_SIZE = 1000;
+	int socks[MAX_SIZE], N;
+	bool checked[MAX_SIZE] = {};
+	// в този масив ще проверяваме дали сме преброили
+	// чорапа на съответния индекс
+
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
+		std::cin >> socks[i];
+
+	for (int i = 0; i < N; i++)
+		if (checked[i] == false)
+		{
+			int count = 0;
+			// колко пъти се среща чорапа с индекс i в масива socks
+
+			for (int j = i; j < N; j++) // търсим от индекс i нататък
+				if (socks[i] == socks[j])
+				{
+					count++;
+					checked[j] = true;  // отбелязваме преброените чорапи от същия модел
+										// за да не ги броим отново
+				}
+
+			if (count % 2 == 1)
+				std::cout << socks[i] << std::endl;
+		}
+}
