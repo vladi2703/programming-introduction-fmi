8b5e4569bf2c068b35ab7962630b33426270f521
Vladimir Stoyanov <46690885+vladi2703@users.noreply.github.com>
2022-12-13 15:28:05 +0200

    Update maxWord-chatGPT.cpp

--- 10-C-strings/solutions/maxWord-chatGPT.cpp
+++ 10-C-strings/solutions/maxWord-chatGPT.cpp
@@ -1,46 +1,45 @@
-/* Решение на опростена версия на задачата maxWord генерирана от AI bot chatGPT. */
+/* Решение на  на задачата maxWord генерирано от AI bot chatGPT.*/
 
+/* Here is a possible implementation of a function that returns the lexicographically 
+biggest word from a C-string, without using excessive memory and without considering the case of the letters: */
 #include <algorithm>
+#include <cstring>
 
 const char* get_biggest_word(const char* str)
 {
     const char* biggest_word = nullptr;
 
     // Split the input string into words
-    const char* words[100];
-    int num_words = 0;
     const char* p = str;
     const char* word_start = p;
     while (*p)
     {
         if (*p == ' ')
         {
-            words[num_words++] = word_start;
+            // Check if the current word is lexicographically bigger than the biggest word found so far
+            if (biggest_word == nullptr || std::strcasecmp(word_start, biggest_word) > 0)
+            {
+                biggest_word = word_start;
+            }
+
             word_start = p + 1;
         }
         ++p;
     }
-    words[num_words++] = word_start;
 
-    // Find the lexicographically biggest word
-    for (int i = 0; i < num_words; ++i)
+    // Check the last word in the input string
+    if (biggest_word == nullptr || std::strcasecmp(word_start, biggest_word) > 0)
     {
-        if (biggest_word == nullptr || std::strcmp(words[i], biggest_word) > 0)
-        {
-            biggest_word = words[i];
-        }
+        biggest_word = word_start;
     }
 
     return biggest_word;
 }
 
-/*
-This function takes in a C-string as input and returns a pointer to the lexicographically biggest word from the input string.
-To do this, it first splits the input string into words using a simple loop.
-It then iterates through the words, keeping track of the lexicographically biggest word found so far. 
-Finally, it returns a pointer to the biggest word.
+/*This function takes in a C-string as input and returns a pointer to the lexicographically biggest word from the input string. 
+To do this, it uses a single loop to iterate through the input string, and checks the lexicographical order of each word in the string as it is encountered. 
+It uses the strcasecmp function from the <cstring> library to compare the words in a case-insensitive manner.
 
-Note that this function has some limitations. 
-For example, it only works for strings that contain up to 100 words, and it does not handle punctuation or other special characters. 
-You may need to modify the function to handle these cases, depending on your specific needs.
-*/
+Note that this function does not store the words in an array, so it does not require excessive memory. 
+However, it does have some limitations. For example, it does not handle punctuation or other special characters.
+You may need to modify the function to handle these cases, depending on your specific needs */
