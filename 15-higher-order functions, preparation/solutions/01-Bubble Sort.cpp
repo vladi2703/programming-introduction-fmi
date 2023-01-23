8137e125c3f3851205cf49dd58d9e35793f37e57
Zahari-M <107109124+Zahari-M@users.noreply.github.com>
2023-01-23 11:15:04 +0200

    Add files via upload

---
+++ 15-higher-order functions, preparation/solutions/01-Bubble Sort.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+
+bool lessOrEq(int a, int b)
+{
+	return a <= b;
+}
+
+
+bool greaterOrEq(int a, int b)
+{
+	return a >= b;
+}
+
+int sumDigits(int a)
+{
+	int sum = 0;
+	while (a > 0)
+	{
+		sum += a % 10;
+		a /= 10;
+	}
+	return sum;
+}
+
+bool compSums(int a, int b)
+{
+	return sumDigits(a) <= sumDigits(b);
+}
+
+bool compLastDigit(int a, int b)
+{
+	return a % 10 >= b % 10;
+}
+
+void bubbleSort(int a[], int n, bool (*comp)(int, int))
+{
+	for (int i = 0; i < n - 1; i++)
+		for (int j = n - 2; j >= i; j--)
+			if (comp(a[j], a[j + 1]) == false)
+			{
+				int tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+}
+
+void print(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+		std::cout << a[i] << " ";
+	std::cout << std::endl;
+}
+
+int main()
+{
+	int a[] = { 9,1111,23,100,624 };
+	bubbleSort(a, 5, greaterOrEq);
+	print(a, 5);
+	bubbleSort(a, 5, lessOrEq);
+	print(a, 5);
+	bubbleSort(a, 5, compSums);
+	print(a, 5);
+	bubbleSort(a, 5, compLastDigit);
+	print(a, 5);
+}
---
+++ 15-higher-order functions, preparation/solutions/02-nice string.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <exception>
+const int maxChar = 128;
+
+void determineString(const char* str)
+{
+	if (*str == '\0')
+		throw std::exception("String is empty");
+
+	int charCount[maxChar] = {};
+
+	for (; *str; str++)
+	{
+		charCount[*str]++;
+	}
+
+	int count1 = 0, count2 = 0;
+	int symbols_with_count1 = 0, symbols_with_count2 = 0;
+	/*
+	Има и по-лесен начин: сортираме масива charCount и анализираме последователностите от еднакви бройки символи
+	със стойност повече от нула. Те трябва да са най-много две, и по-голямата бройка трябва да е по-голяма с 1
+	от другите и да е само една.
+	Това не влияе на сложността, тъй като charCount има константен размер, и сортирането му също отнема константна сложност
+	Общата сложност на алгоритъма остава линейна заради обхождането на низа.
+	*/
+
+	for (int i = 0; i < maxChar; i++)
+		if (charCount[i] != 0)
+		{
+			if (count1 == 0)
+			{
+				count1 = charCount[i];
+				symbols_with_count1++;
+			}
+			else if (charCount[i] == count1)
+				symbols_with_count1++;
+			else if (count2 == 0)
+			{
+				count2 = charCount[i];
+				symbols_with_count2++;
+			}
+			else if (charCount[i] == count2)
+				symbols_with_count2++;
+			else
+			{
+				std::cout << "bad string" << std::endl;
+				return;
+			}
+		}
+
+	if (count2 == 0)
+		std::cout << "nice string" << std::endl;
+	else if ((count1 == count2 + 1 && symbols_with_count1 == 1) ||
+		(count2 == count1 + 1 && symbols_with_count2 == 1))
+		std::cout << "weird string" << std::endl;
+	else
+		std::cout << "bad string" << std::endl;
+
+}
+
+int main()
+{
+	determineString("aaaa bbb  ...");
+	determineString("aaa bbb  ...");
+	determineString("aaaa bbbb  ...");
+	determineString("aaaazzz");
+	determineString("aaaazzzdd");
+}
